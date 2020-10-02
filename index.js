bd0e339bbcb48a0d9a81c0bf19094cf9e35bb7d1
Mfuon Leonard <mfolee@gmail.com>
2020-10-02 16:53:37 +0300

    second x

---
+++ index.js
@@ -0,0 +1,36 @@
+
+const m = require('md5');
+
+let first = `npm ERR! code ENOSELF
+npm ERR! Refusing to install package with name "react-mui-datatables" under a package
+npm ERR! also called "react-mui-datatables". Did you name your project the same
+npm ERR! as the dependency you're installing?
+npm ERR!
+npm ERR! For more information, see:
+npm ERR!     <https://docs.npmjs.com/cli/install#limitations-of-npms-install-algorithm>
+
+npm ERR! A complete log of this run can be found in:`
+
+let second = `npm ERR! code ENOSELF
+npm ERR! Refusing to install package with name "react-mui-datatables" under a package
+npm ERR! also called "react-mui-datatables". Did you name your project the same
+npm ERR! as the dependency you're installing?
+npm ERR!
+npm ERR! For more information, see:
+npm ERR!     <https://docs.npmjs.com/cli/install#limitations-of-npms-install-algorithm>
+
+npm ERR! A complete log of this run can be found in:`;
+
+function compareLongString(stringOne,stringTwo){
+    var one = m(stringOne);
+    var two = m(stringTwo);
+    if(one === two){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+var results = compareLongString(first,second);
+
+console.log(results);
